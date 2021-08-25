1ae214d5435040dba25524a351f2b5ecc3f061bc
Terry <terry.kim@berkeley.edu>
2021-08-25 17:12:41 +0900

    meetup app finished

---
+++ meet-up-project/src/pages/NewMeetups.js
@@ -0,0 +1,31 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+
+
+function NewMeetupsPage() {
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'REPLACE WITH YOUR FIREBASE url',
+            {
+                method: 'POST', 
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            history.replace('/');
+        });
+    }
+
+    return (<section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm onAddMeetup = {addMeetupHandler} />
+        </section>
+    );
+}
+export default NewMeetupsPage;
